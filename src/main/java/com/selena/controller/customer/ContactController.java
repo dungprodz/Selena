53fb9acb1a1ff4cb882ec4a0209bc06a08a52825
DungNT2 <nguyentiendunga42002@gmail.com>
2023-06-15 14:21:35 +0700

    update bill

--- src/main/java/com/selena/controller/customer/ContactController.java
+++ src/main/java/com/selena/controller/customer/ContactController.java
@@ -1,6 +1,8 @@
 package com.selena.controller.customer;
 
-import com.selena.model.Product;
+import com.selena.model.Contact;
+import com.selena.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +11,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ContactController {
+    @Autowired
+    private ContactRepository contactRepository;
 
-    @RequestMapping(value={"/contact"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/contact"}, method = RequestMethod.GET)
     public String contact(final Model model,
-                                final HttpServletRequest request,
-                                final HttpServletResponse response
+                          final HttpServletRequest request,
+                          final HttpServletResponse response
     ) throws IOException {
 
-
+        List<Contact> contacts = contactRepository.findAll().stream().sorted(Comparator.comparing(Contact::getId).reversed()).collect(Collectors.toList());
+        model.addAttribute("contact", contacts);
         return "customer/contact";
     }
 }
